2a9e75a8ee07fe55ab425b9aef9ce3f1c11a9a23
Alex Massenzio <alexmassenzio@gmail.com>
2016-11-24 01:08:01 -0500

    added unity analytics and fixed more UI issues

---
+++ Assets/Scripts/AnalyticsManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class AnalyticsManager : MonoBehaviour {
+
+	private static AnalyticsManager instance = null;
+	public static AnalyticsManager Instance
+	{
+		get { return instance; }
+	}
+
+	private int levelNumber;
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start()
+	{
+		levelNumber = SceneManager.GetActiveScene().buildIndex;
+		ReportLevelStart(levelNumber);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(levelNumber != SceneManager.GetActiveScene().buildIndex)
+		{
+			levelNumber = SceneManager.GetActiveScene().buildIndex;
+			ReportLevelStart(levelNumber);
+		}
+	}
+
+	private void ReportLevelStart(int l)
+	{
+		Analytics.CustomEvent("levelStart", new Dictionary<string, object>
+		{
+			{"levelNumber", l}
+		});
+		Debug.Log("Sent Analytics data: Entered Level: " + levelNumber);
+	}
+}
